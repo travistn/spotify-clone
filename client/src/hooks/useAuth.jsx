6a3429fa2cdea5b9954bccbdf3e44fe11901453b
travistn <travisn714@gmail.com>
2022-08-02 17:25:44 -0700

    Created a custom hook that authentifies to use the Spotify api and refreshes the access token timer if time runs out

---
+++ client/src/hooks/useAuth.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useAuth = (code) => {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const {
+          data: { accessToken, refreshToken, expiresIn },
+        } = await axios.post(`${process.env.REACT_APP_BASE_URL}/login`, { code });
+        setAccessToken(accessToken);
+        setRefreshToken(refreshToken);
+        setExpiresIn(expiresIn);
+        window.history.pushState({}, null, '/');
+      } catch {
+        window.location = '/';
+      }
+    })();
+  }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+
+    const interval = setInterval(async () => {
+      try {
+        const {
+          data: { accessToken, expiresIn },
+        } = await axios.post(`${process.env.REACT_APP_BASE_URL}/refresh`, { refreshToken });
+        setAccessToken(accessToken);
+        setExpiresIn(expiresIn);
+      } catch {
+        window.location = '/';
+      }
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
+};
+
+export default useAuth;
