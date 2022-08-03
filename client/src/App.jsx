e7bc4a32546d3aa874a4014693ddb76678d7c415
travistn <travisn714@gmail.com>
2022-08-02 17:28:50 -0700

    Added dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Login from './components/Login/Login';
+import Dashboard from './containers/Dashboard/Dashboard';
+
+const code = new URLSearchParams(window.location.search).get('code');
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path='/' />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </>
-  );
+  return code ? <Dashboard code={code} /> : <Login />;
 };
 
 export default App;
