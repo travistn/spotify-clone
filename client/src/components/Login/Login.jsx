c3dcc330aac57bdb34fb767d5f2dfcda9baad6ca
travistn <travisn714@gmail.com>
2022-08-22 15:57:57 -0700

    Implemented login authentication

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BsSpotify } from 'react-icons/bs';
 
 import './Login.css';
+import logo from '../../assets/logo.png';
 
 const Login = () => {
   const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
@@ -9,12 +9,11 @@ const Login = () => {
   return (
     <div className='login__container'>
       <div className='login__header'>
-        <BsSpotify className='spotify-icon' />
-        <h1>Spotify</h1>
+        <img src={logo} alt='spotify-logo' />
       </div>
       <div className='login-button'>
         <button>
-          <a href={AUTH_URL}>Log In</a>
+          <a href={AUTH_URL}>Log In With Spotify</a>
         </button>
       </div>
     </div>
