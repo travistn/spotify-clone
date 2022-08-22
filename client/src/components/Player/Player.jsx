cf111795b85304eeadc17b79dab7c053f95f8918
travistn <travisn714@gmail.com>
2022-08-22 15:53:54 -0700

    Implemented a player to play songs

--- client/src/components/Player/Player.jsx
+++ client/src/components/Player/Player.jsx
@@ -1,22 +1,38 @@
 import { useState, useEffect } from 'react';
 import SpotifyPlayer from 'react-spotify-web-playback';
 
-const Player = ({ accessToken, trackUri }) => {
+import './Player.css';
+
+const Player = ({ accessToken, track }) => {
   const [play, setPlay] = useState(false);
 
-  useEffect(() => setPlay(true), [trackUri]);
+  useEffect(() => setPlay(true), [track?.uri]);
 
   if (!accessToken) return null;
   return (
-    <SpotifyPlayer
-      token={accessToken}
-      showSaveIcon
-      callback={(state) => {
-        if (!state.isPlaying) setPlay(false);
-      }}
-      play={play}
-      uris={trackUri ? [trackUri] : []}
-    />
+    <div className='player'>
+      <SpotifyPlayer
+        token={accessToken}
+        showSaveIcon
+        callback={(state) => {
+          if (!state.isPlaying) setPlay(false);
+        }}
+        play={play}
+        uris={track?.uri ? [track?.uri] : []}
+        styles={{
+          bgColor: '#181818',
+          color: 'white',
+          trackArtistColor: 'gray',
+          trackNameColor: 'white',
+          sliderColor: 'white',
+          sliderHandleColor: 'white',
+          sliderTrackColor: 'gray',
+          activeColor: '#00d76b',
+          height: '100px',
+        }}
+        initialVolume={100}
+      />
+    </div>
   );
 };
 
