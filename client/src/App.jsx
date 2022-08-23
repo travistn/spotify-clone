abb40cd4f58beb614f17b1b8a2c9d669edd35b0f
travistn <travisn714@gmail.com>
2022-08-23 16:38:00 -0700

    Implemented useContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Login from './components/Login/Login';
 import Dashboard from './containers/Dashboard/Dashboard';
+import Search from './containers/Search/Search';
+import { AppContext } from './contexts/AppContext';
 
 const code = new URLSearchParams(window.location.search).get('code');
 
 const App = () => {
-  return code ? <Dashboard code={code} /> : <Login />;
+  const [playingTrack, setPlayingTrack] = useState();
+
+  const contextValues = { playingTrack };
+
+  return code ? (
+    <AppContext.Provider value={contextValues}>
+      <Routes>
+        <Route path='/' element={<Dashboard code={code} />}>
+          <Route path='search' element={<Search setPlayingTrack={setPlayingTrack} />} />
+        </Route>
+      </Routes>
+    </AppContext.Provider>
+  ) : (
+    <Login />
+  );
 };
 
 export default App;
