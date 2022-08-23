fdc83b17d82e8a6fa31cb4e5abdad1ffa3853323
travistn <travisn714@gmail.com>
2022-08-23 15:22:43 -0700

    Created a nav sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { AiFillHome } from 'react-icons/ai';
 import { FiSearch } from 'react-icons/fi';
 import { VscLibrary } from 'react-icons/vsc';
@@ -6,22 +7,37 @@ import { VscLibrary } from 'react-icons/vsc';
 import './Sidebar.css';
 import logo from '../../assets/logo-white.png';
 
+const linkStyles = {
+  fontSize: '15px',
+  fontWeight: '700',
+  textDecoration: 'none',
+  color: 'var(--color-gray)',
+};
+
 const Sidebar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='sidebar__container'>
-      <img src={logo} alt='spotify-logo' className='sidebar-logo' />
+      <img src={logo} alt='spotify-logo' className='sidebar-logo' onClick={() => navigate('/')} />
       <div className='sidebar__links'>
         <div className='sidebar-link'>
           <AiFillHome className='sidebar-homeIcon' />
-          <h5>Home</h5>
+          <Link to='/' style={linkStyles}>
+            Home
+          </Link>
         </div>
         <div className='sidebar-link'>
           <FiSearch className='sidebar-searchIcon' />
-          <h5>Search</h5>
+          <Link to='/search' style={linkStyles}>
+            Search
+          </Link>{' '}
         </div>
         <div className='sidebar-link'>
           <VscLibrary className='sidebar-libraryIcon' />
-          <h5>Your Library</h5>
+          <Link to='/library' style={linkStyles}>
+            Your Library
+          </Link>{' '}
         </div>
       </div>
     </div>
