257405e2e74862f0f4275f83ca5d968d2d432f76
travistn <travisn714@gmail.com>
2022-08-25 14:12:42 -0700

    Added a scope for api usage

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -4,7 +4,7 @@ import './Login.css';
 import logo from '../../assets/logo.png';
 
 const Login = () => {
-  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-recently-played`;
 
   return (
     <div className='login__container'>
