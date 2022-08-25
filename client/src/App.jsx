77850bafad3151900f08e3b1ca90ceb0d192426d
travistn <travisn714@gmail.com>
2022-08-25 14:12:17 -0700

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,13 @@
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
 
 import './App.css';
 import Login from './components/Login/Login';
 import Dashboard from './containers/Dashboard/Dashboard';
-import Search from './containers/Search/Search';
-import { AppContext } from './contexts/AppContext';
 
 const code = new URLSearchParams(window.location.search).get('code');
 
 const App = () => {
-  const [playingTrack, setPlayingTrack] = useState();
-
-  const contextValues = { playingTrack };
-
-  return code ? (
-    <AppContext.Provider value={contextValues}>
-      <Routes>
-        <Route path='/' element={<Dashboard code={code} />}>
-          <Route path='search' element={<Search setPlayingTrack={setPlayingTrack} />} />
-        </Route>
-      </Routes>
-    </AppContext.Provider>
-  ) : (
-    <Login />
-  );
+  return code ? <Dashboard code={code} /> : <Login />;
 };
 
 export default App;
