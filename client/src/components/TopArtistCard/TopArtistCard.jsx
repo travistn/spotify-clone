f354b0c4be21be45b7a8de1e9193a4e0da2c036b
travistn <travisn714@gmail.com>
2022-09-06 14:49:26 -0700

    Clicking on an artist card will direct to the artist page

--- client/src/components/TopArtistCard/TopArtistCard.jsx
+++ client/src/components/TopArtistCard/TopArtistCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './TopArtistCard.css';
 
 const TopArtistCard = ({ artist }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className='topArtistCard'>
+    <div className='topArtistCard' onClick={() => navigate(`/artist/${artist?.id}`)}>
       <img src={artist?.images[0].url} alt='artist-cover' />
       <div className='topArtistCard__info'>
         <h4>{artist?.name}</h4>
