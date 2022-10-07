f3d1cade8cbdd95f9ed11b81003704a67789e790
travistn <travisn714@gmail.com>
2022-10-07 14:54:15 -0700

    Added an artists and albums section in the search page

--- client/src/containers/Search/Search.jsx
+++ client/src/containers/Search/Search.jsx
@@ -4,21 +4,24 @@ import { useNavigate } from 'react-router-dom';
 import './Search.css';
 import Searchbar from '../../components/Searchbar/Searchbar';
 import PlaylistCard from '../../components/PlaylistCard/PlaylistCard';
+import ArtistCard from '../../components/ArtistCard/ArtistCard';
+import AlbumCard from '../../components/AlbumCard/AlbumCard';
 import { spotifyApi } from '../../reuseables/SpotifyApi';
 
 const Search = ({ chooseTrack }) => {
   const [search, setSearch] = useState('');
-  const [searchArtist, setSearchArtist] = useState();
+  const [searchArtists, setSearchArtists] = useState();
   const [searchSongs, setSearchSongs] = useState();
   const [searchPlaylists, setSearchPlaylists] = useState();
+  const [searchAlbums, setSearchAlbums] = useState();
   const [searchLoaded, setSearchLoaded] = useState(false);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     spotifyApi
-      .searchArtists(search, { limit: 1 })
-      .then((res) => setSearchArtist(res.body.artists.items[0]));
+      .searchArtists(search, { limit: 7 })
+      .then((res) => setSearchArtists(res.body.artists.items));
   }, [search]);
 
   useEffect(() => {
@@ -34,6 +37,12 @@ const Search = ({ chooseTrack }) => {
   }, [search]);
 
   useEffect(() => {
+    spotifyApi
+      .searchAlbums(search, { limit: 7 })
+      .then((res) => setSearchAlbums(res.body.albums.items));
+  }, [search]);
+
+  useEffect(() => {
     if (search === '') setSearchLoaded(false);
   }, [search]);
 
@@ -41,51 +50,67 @@ const Search = ({ chooseTrack }) => {
     <div className='search__container'>
       <Searchbar search={search} setSearch={setSearch} setSearchLoaded={setSearchLoaded} />
       {searchLoaded && (
-        <div className='search__topResults'>
-          <div className='search__topResults-artist'>
-            <h4>Top Result</h4>
-            <div
-              className='search__topResults-artistCard'
-              onClick={() => navigate(`/artist/${searchArtist?.id}`)}>
-              <img src={searchArtist?.images[0].url} alt='artist-profile' />
-              <h1>{searchArtist?.name}</h1>
-              <button>{searchArtist?.type}</button>
+        <>
+          <div className='search__topResults'>
+            <div className='search__topResults-artist'>
+              <h4>Top Result</h4>
+              <div
+                className='search__topResults-artistCard'
+                onClick={() => navigate(`/artist/${searchArtists?.[0]?.id}`)}>
+                <img src={searchArtists?.[0]?.images[0].url} alt='artist-profile' />
+                <h1>{searchArtists?.[0]?.name}</h1>
+                <button>{searchArtists?.[0]?.type}</button>
+              </div>
             </div>
-          </div>
-          <div className='search__topResults-songs'>
-            <h4>Songs</h4>
-            <div className='search__topResults-songsList'>
-              {searchSongs?.map((song) => (
-                <div className='search__topResults-songCard'>
-                  <img
-                    src={song?.album.images[0].url}
-                    alt='album-cover'
-                    onClick={() => chooseTrack(song)}
-                  />
-                  <div className='search__topResults-songCard__content'>
-                    <h5 onClick={() => chooseTrack(song)}>{song?.name}</h5>
-                    <p onClick={() => navigate(`/artist/${song?.artists[0].id}`)}>
-                      {song?.artists[0].name}
+            <div className='search__topResults-songs'>
+              <h4>Songs</h4>
+              <div className='search__topResults-songsList'>
+                {searchSongs?.map((song) => (
+                  <div className='search__topResults-songCard'>
+                    <img
+                      src={song?.album.images[0].url}
+                      alt='album-cover'
+                      onClick={() => chooseTrack(song)}
+                    />
+                    <div className='search__topResults-songCard__content'>
+                      <h5 onClick={() => chooseTrack(song)}>{song?.name}</h5>
+                      <p onClick={() => navigate(`/artist/${song?.artists[0].id}`)}>
+                        {song?.artists[0].name}
+                      </p>
+                    </div>
+                    <p className='search__topResults-songCard-trackTime'>
+                      {new Date(song?.duration_ms).toISOString().slice(14, 19)}
                     </p>
                   </div>
-                  <p className='search__topResults-songCard-trackTime'>
-                    {new Date(song?.duration_ms).toISOString().slice(14, 19)}
-                  </p>
-                </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className='search-artists__container'>
+            <h2>Artists</h2>
+            <div className='search-artists'>
+              {searchArtists?.map((artist) => (
+                <ArtistCard artist={artist} />
               ))}
             </div>
           </div>
-        </div>
-      )}
-      {searchLoaded && (
-        <div className='search-playlists__container'>
-          <h2>Playlists</h2>
-          <div className='search-playlists'>
-            {searchPlaylists?.map((playlist) => (
-              <PlaylistCard playlist={playlist} chooseTrack={chooseTrack} />
-            ))}
+          <div className='search-albums__container'>
+            <h2>Albums</h2>
+            <div className='search-albums'>
+              {searchAlbums?.map((album) => (
+                <AlbumCard album={album} />
+              ))}
+            </div>
           </div>
-        </div>
+          <div className='search-playlists__container'>
+            <h2>Playlists</h2>
+            <div className='search-playlists'>
+              {searchPlaylists?.map((playlist) => (
+                <PlaylistCard playlist={playlist} chooseTrack={chooseTrack} />
+              ))}
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
