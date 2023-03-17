ab65ee60910a8e24fd748a58fdac0694f024078a
travistn <travisn714@gmail.com>
2023-03-16 19:52:08 -0700

    Added env variable

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.post('/login', async (req, res) => {
   const { code } = req.body;
