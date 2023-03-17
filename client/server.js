b5cc3533a401f963cc312c9b511ec9d171c0c30b
travistn <travisn714@gmail.com>
2023-03-16 22:01:23 -0700

    Moved express server into client folder

---
+++ client/server.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3001;
+
+app.post('/login', async (req, res) => {
+  const { code } = req.body;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REACT_APP_REDIRECT_URI,
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+  });
+
+  try {
+    const {
+      body: { access_token, refresh_token, expires_in },
+    } = await spotifyApi.authorizationCodeGrant(code);
+
+    res.json({ access_token, refresh_token, expires_in });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+});
+
+app.post('/refresh', async (req, res) => {
+  const { refreshToken } = req.body;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REACT_APP_REDIRECT_URI,
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+    refreshToken,
+  });
+
+  try {
+    const {
+      body: { access_token, expires_in },
+    } = await spotifyApi.refreshAccessToken();
+    res.json({ access_token, expires_in });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+});
+
+app.listen(port, (err) => {
+  if (err) console.log(err);
+  console.log(`Listening on port ${port}`);
+});
